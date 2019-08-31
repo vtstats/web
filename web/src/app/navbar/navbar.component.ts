decefe348e820322d95af4dcdc8ee6a80123a092
PoiScript <poiscript@gmail.com>
2019-08-31 22:19:52 +0800

    feat(web): use svg icon

--- web/src/app/navbar/navbar.component.ts
+++ web/src/app/navbar/navbar.component.ts
@@ -1,20 +1,8 @@
 import { Component } from "@angular/core";
-import { DomSanitizer } from "@angular/platform-browser";
-import { MatIconRegistry } from "@angular/material/icon";
 
 @Component({
   selector: "hs-navbar",
   templateUrl: "./navbar.component.html",
   styleUrls: ["./navbar.component.scss"]
 })
-export class NavbarComponent {
-  constructor(
-    private iconRegistry: MatIconRegistry,
-    private sanitizer: DomSanitizer
-  ) {
-    this.iconRegistry.addSvgIcon(
-      "holo-stats",
-      this.sanitizer.bypassSecurityTrustResourceUrl("assets/logo.svg")
-    );
-  }
-}
+export class NavbarComponent {}
