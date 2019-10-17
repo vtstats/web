79b06a6bf9d57a72fb1c2432c390bff12bef7327
PoiScript <poiscript@gmail.com>
2019-10-17 14:43:07 +0800

    fix(web): encode image link url

--- web/src/app/quotes/quotes.component.ts
+++ web/src/app/quotes/quotes.component.ts
@@ -1,10 +1,14 @@
 import { Component } from "@angular/core";
 
 @Component({
-  selector: "app-quotes",
+  selector: "hs-quotes",
   templateUrl: "./quotes.component.html",
   styleUrls: ["./quotes.component.scss"]
 })
 export class QuotesComponent {
   readonly quotes = ["阿火", "yagoo落泪"];
+
+  encodeURI(s: string): string {
+    return encodeURI(s);
+  }
 }
