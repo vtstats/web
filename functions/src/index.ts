5de151ab5404b78da630a55b46c7c7e877f91e4b
PoiScript <poiscript@gmail.com>
2020-01-25 23:39:49 +0800

    feat(functions): validate http query and params

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import {
   compareDesc,
   getUnixTime,
@@ -5,149 +6,146 @@ import {
   isBefore,
   parseISO
 } from "date-fns";
-import * as express from "express";
+import express from "express";
 import * as functions from "firebase-functions";
-import * as cors from "cors";
 
 import { Database } from "./database";
 import { writeLog } from "./logging";
+import {
+  validate,
+  validateEndAtQuery,
+  validateIdParam,
+  validateIdsQuery,
+  validateStartAtQuery
+} from "./validate";
 
 const app = express();
 const db = new Database();
 
 app.use(cors({ origin: "http://localhost:4200", methods: "GET" }));
 
-app.get("/api_v2/youtube_channel", async (req, res) => {
-  const ids: string[] =
-    req.query.ids && typeof req.query.ids == "string"
-      ? req.query.ids.split(",")
-      : [];
+app.get(
+  "/api_v2/youtube_channel",
+  validate([validateIdsQuery]),
+  async (req, res) => {
+    const ids: string[] = req.query.ids;
 
-  const { updatedAt, vtubers } = await db.getVTubers();
+    const { updatedAt, vtubers } = await db.getVTubers();
 
-  const filtered = ids.map(id => ({
-    id,
-    subs: vtubers[id].youtubeStats.subs,
-    dailySubs: vtubers[id].youtubeStats.dailySubs,
-    weeklySubs: vtubers[id].youtubeStats.weeklySubs,
-    monthlySubs: vtubers[id].youtubeStats.monthlySubs,
-    views: vtubers[id].youtubeStats.views,
-    dailyViews: vtubers[id].youtubeStats.dailyViews,
-    weeklyViews: vtubers[id].youtubeStats.weeklyViews,
-    monthlyViews: vtubers[id].youtubeStats.monthlyViews
-  }));
+    const filtered = ids.map(id => ({
+      id,
+      subs: vtubers[id].youtubeStats.subs,
+      dailySubs: vtubers[id].youtubeStats.dailySubs,
+      weeklySubs: vtubers[id].youtubeStats.weeklySubs,
+      monthlySubs: vtubers[id].youtubeStats.monthlySubs,
+      views: vtubers[id].youtubeStats.views,
+      dailyViews: vtubers[id].youtubeStats.dailyViews,
+      weeklyViews: vtubers[id].youtubeStats.weeklyViews,
+      monthlyViews: vtubers[id].youtubeStats.monthlyViews
+    }));
 
-  res.json({ updatedAt: updatedAt.toISOString(), channels: filtered });
+    res.json({ updatedAt: updatedAt.toISOString(), channels: filtered });
 
-  writeLog({
-    event: "http",
-    value: "/api_v2/youtube_channel",
-    message: `ids: ${ids}`
-  });
-});
+    writeLog({
+      event: "http",
+      value: "/api_v2/youtube_channel",
+      message: `ids: ${ids}`
+    });
+  }
+);
 
-app.get("/api_v2/bilibili_channel", async (req, res) => {
-  const ids: string[] =
-    req.query.ids && typeof req.query.ids == "string"
-      ? req.query.ids.split(",")
-      : [];
+app.get(
+  "/api_v2/bilibili_channel",
+  validate([validateIdsQuery]),
+  async (req, res) => {
+    const ids: string[] = req.query.ids;
 
-  const { updatedAt, vtubers } = await db.getVTubers();
+    const { updatedAt, vtubers } = await db.getVTubers();
 
-  const filtered = ids.map(id => ({
-    id,
-    subs: vtubers[id].bilibiliStats.subs,
-    dailySubs: vtubers[id].bilibiliStats.dailySubs,
-    weeklySubs: vtubers[id].bilibiliStats.weeklySubs,
-    monthlySubs: vtubers[id].bilibiliStats.monthlySubs,
-    views: vtubers[id].bilibiliStats.views,
-    dailyViews: vtubers[id].bilibiliStats.dailyViews,
-    weeklyViews: vtubers[id].bilibiliStats.weeklyViews,
-    monthlyViews: vtubers[id].bilibiliStats.monthlyViews
-  }));
+    const filtered = ids.map(id => ({
+      id,
+      subs: vtubers[id].bilibiliStats.subs,
+      dailySubs: vtubers[id].bilibiliStats.dailySubs,
+      weeklySubs: vtubers[id].bilibiliStats.weeklySubs,
+      monthlySubs: vtubers[id].bilibiliStats.monthlySubs,
+      views: vtubers[id].bilibiliStats.views,
+      dailyViews: vtubers[id].bilibiliStats.dailyViews,
+      weeklyViews: vtubers[id].bilibiliStats.weeklyViews,
+      monthlyViews: vtubers[id].bilibiliStats.monthlyViews
+    }));
 
-  res.json({ updatedAt: updatedAt.toISOString(), channels: filtered });
+    res.json({ updatedAt: updatedAt.toISOString(), channels: filtered });
 
-  writeLog({
-    event: "http",
-    value: "/api_v2/bilibili_channel",
-    message: `ids: ${ids}`
-  });
-});
+    writeLog({
+      event: "http",
+      value: "/api_v2/bilibili_channel",
+      message: `ids: ${ids}`
+    });
+  }
+);
 
-app.get("/api_v2/vtuber/:id", async (req, res) => {
-  const id: string = req.params.id;
+app.get(
+  "/api_v2/vtuber/:id",
+  validate([validateIdParam, validateStartAtQuery, validateEndAtQuery]),
+  async (req, res) => {
+    const id: string = req.params.id;
+    const startAt: Date = req.query.startAt;
+    const endAt: Date = req.query.endAt;
 
-  const startAt: Date =
-    req.query.startAt && typeof req.query.startAt == "string"
-      ? parseISO(req.query.startAt)
-      : new Date(0);
+    const startTimestamp = getUnixTime(startAt).toString();
+    const endTimestamp = getUnixTime(endAt).toString();
 
-  const endAt: Date =
-    req.query.endAt && typeof req.query.endAt == "string"
-      ? parseISO(req.query.endAt)
-      : new Date();
+    const { vtubers } = await db.getVTubers();
+    const stats = await db.getVTuberStat(id);
 
-  const startTimestamp = getUnixTime(startAt).toString();
-  const endTimestamp = getUnixTime(endAt).toString();
+    const filtered = Object.keys(stats)
+      .filter(key => startTimestamp <= key && key <= endTimestamp)
+      .reduce((obj, key) => ({ ...obj, [key]: stats[key] }), {});
 
-  const { vtubers } = await db.getVTubers();
-  const stats = await db.getVTuberStat(id);
+    res.json({ vtuber: vtubers[id], series: filtered });
 
-  const filtered = Object.keys(stats)
-    .filter(key => startTimestamp <= key && key <= endTimestamp)
-    .reduce((obj, key) => ({ ...obj, [key]: stats[key] }), {});
+    writeLog({
+      event: "http",
+      value: `/api_v2/vtuber/${id}`,
+      message: `startAt: ${startAt.toISOString()}, endAt: ${endAt.toISOString()}`
+    });
+  }
+);
 
-  res.json({ vtuber: vtubers[id], series: filtered });
+app.get(
+  "/api_v2/youtube_stream",
+  validate([validateIdsQuery, validateStartAtQuery, validateEndAtQuery]),
+  async (req, res) => {
+    const ids: string[] = req.query.ids;
+    const startAt: Date = req.query.startAt;
+    const endAt: Date = req.query.endAt;
 
-  writeLog({
-    event: "http",
-    value: `/api_v2/vtuber/${id}`,
-    message: `startAt: ${startAt.toISOString()}, endAt: ${endAt.toISOString()}`
-  });
-});
+    const { streams, updatedAt } = await db.getStreams();
 
-app.get("/api_v2/youtube_stream", async (req, res) => {
-  const ids: string[] =
-    req.query.ids && typeof req.query.ids == "string"
-      ? req.query.ids.split(",")
-      : [];
+    const filtered = Object.values(streams)
+      .filter(stream => ids.includes(stream.vtuberId))
+      .filter(
+        stream =>
+          isAfter(parseISO(stream.start), startAt) &&
+          isBefore(parseISO(stream.start), endAt)
+      )
+      .sort((a, b) => compareDesc(parseISO(a.start), parseISO(b.start)));
 
-  const startAt: Date =
-    req.query.startAt && typeof req.query.startAt == "string"
-      ? parseISO(req.query.startAt)
-      : new Date(0);
+    res.json({
+      updatedAt: updatedAt.toISOString(),
+      streams: filtered.slice(0, 24),
+      hasMore: filtered.length > 24
+    });
 
-  const endAt: Date =
-    req.query.endAt && typeof req.query.endAt == "string"
-      ? parseISO(req.query.endAt)
-      : new Date();
+    writeLog({
+      event: "http",
+      value: "/api_v2/youtube_stream",
+      message: `ids: ${ids}, startAt: ${startAt.toISOString()}, endAt: ${endAt.toISOString()}`
+    });
+  }
+);
 
-  const { streams, updatedAt } = await db.getStreams();
-
-  const filtered = Object.values(streams)
-    .filter(stream => ids.includes(stream.vtuberId))
-    .filter(
-      stream =>
-        isAfter(parseISO(stream.start), startAt) &&
-        isBefore(parseISO(stream.start), endAt)
-    )
-    .sort((a, b) => compareDesc(parseISO(a.start), parseISO(b.start)));
-
-  res.json({
-    updatedAt: updatedAt.toISOString(),
-    streams: filtered.slice(0, 24),
-    hasMore: filtered.length > 24
-  });
-
-  writeLog({
-    event: "http",
-    value: "/api_v2/youtube_stream",
-    message: `ids: ${ids}, startAt: ${startAt.toISOString()}, endAt: ${endAt.toISOString()}`
-  });
-});
-
-app.get("/api_v2/stream/:id", async (req, res) => {
+app.get("/api_v2/stream/:id", validate([validateIdParam]), async (req, res) => {
   const id: string = req.params.id;
 
   const { streams } = await db.getStreams();
---
+++ functions/src/validate.ts
@@ -0,0 +1,47 @@
+import { parseISO } from "date-fns";
+import { NextFunction, Request, Response } from "express";
+import {
+  param,
+  query,
+  ValidationChain,
+  validationResult
+} from "express-validator";
+import vtubers from "../../vtubers.json";
+
+const vtuber_ids: string[] = [];
+
+for (const item of vtubers.items) {
+  for (const member of item.members) {
+    vtuber_ids.push(member.id);
+  }
+}
+
+export const validate = (validations: ValidationChain[]) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    await Promise.all(validations.map(validation => validation.run(req)));
+
+    const errors = validationResult(req);
+    if (errors.isEmpty()) {
+      return next();
+    } else {
+      res.status(422).json({ errors: errors.array() });
+    }
+  };
+};
+
+export const validateIdParam = param("id")
+  .isString()
+  .custom((id: string) => vtuber_ids.includes(id));
+
+export const validateIdsQuery = query("ids")
+  .isString()
+  .customSanitizer((val: string) => val.split(","))
+  .custom((ids: string[]) => ids.every(id => vtuber_ids.includes(id)));
+
+export const validateStartAtQuery = query("startAt")
+  .isISO8601()
+  .customSanitizer((val: string) => parseISO(val));
+
+export const validateEndAtQuery = query("endAt")
+  .isISO8601()
+  .customSanitizer((val: string) => parseISO(val));
