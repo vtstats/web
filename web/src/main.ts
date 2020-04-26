15fab2e956eff97c05b3b2636ed76ad3dd0eeecd
PoiScript <poiscript@gmail.com>
2020-04-26 19:22:14 +0800

    feat(web): import translations before components

--- web/src/main.ts
+++ web/src/main.ts
@@ -1,11 +1,13 @@
 import { enableProdMode } from "@angular/core";
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
 
+// load translations before importing any components,
+// $localize requries these translations to work.
+import "./i18n";
+
 import { AppModule } from "./app/app.module";
 import { ENABLE_DARK_MODE } from "./app/services/config";
 import { environment } from "./environments/environment";
-
-import "./i18n";
 
 if (localStorage.getItem(ENABLE_DARK_MODE) !== null) {
   document.body.classList.add("dark");
