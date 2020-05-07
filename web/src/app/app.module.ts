5ed132bcea597ec0acc82b76b7becfd516b1001f
PoiScript <poiscript@gmail.com>
2020-05-07 15:19:00 +0800

    feat(web): use ngx-ellipsis

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { BrowserModule } from "@angular/platform-browser";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { RouterModule, Routes, PreloadAllModules } from "@angular/router";
 import { ServiceWorkerModule } from "@angular/service-worker";
+import { EllipsisModule } from "ngx-ellipsis";
 
 import { environment } from "../environments/environment";
 
@@ -121,6 +122,7 @@ const ROUTES: Routes = [
     MatTooltipModule,
     MatTreeModule,
     SharedModule,
+    EllipsisModule,
     RouterModule.forRoot(ROUTES, { preloadingStrategy: PreloadAllModules }),
     ServiceWorkerModule.register("ngsw-worker.js", {
       enabled: environment.production,
