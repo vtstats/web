55de773cd5088c61039e8d8216127e4005880ff9
PoiScript <poiscript@gmail.com>
2020-08-06 16:13:54 +0800

    feat(server): update initial.sql

--- server/sql/initial.sql
+++ server/sql/initial.sql
@@ -1,242 +1,94 @@
---- アキ・ローゼンタール
-INSERT INTO youtube_channels (vtuber_id) VALUES ('aki');
-
---- 湊あくあ
-INSERT INTO youtube_channels (vtuber_id) VALUES ('aqua');
-
---- 百鬼あやめ
-INSERT INTO youtube_channels (vtuber_id) VALUES ('ayame');
-
---- AZKi
-INSERT INTO youtube_channels (vtuber_id) VALUES ('azki');
-
---- 癒月ちょこ
-INSERT INTO youtube_channels (vtuber_id) VALUES ('choco');
-
---- 癒月ちょこ alt
-INSERT INTO youtube_channels (vtuber_id) VALUES ('choco_alt');
-
---- 桐生ココ
-INSERT INTO youtube_channels (vtuber_id) VALUES ('coco');
-
---- 不知火フレア
-INSERT INTO youtube_channels (vtuber_id) VALUES ('flare');
-
---- 白上フブキ
-INSERT INTO youtube_channels (vtuber_id) VALUES ('fubuki');
-
---- 赤井はあと
-INSERT INTO youtube_channels (vtuber_id) VALUES ('haato');
-
---- 姫森ルーナ
-INSERT INTO youtube_channels (vtuber_id) VALUES ('himemoriluna');
-
---- hololive
-INSERT INTO youtube_channels (vtuber_id) VALUES ('hololive');
-
---- 天音かなた
-INSERT INTO youtube_channels (vtuber_id) VALUES ('kanata');
-
---- 戌神ころね
-INSERT INTO youtube_channels (vtuber_id) VALUES ('korone');
-
---- 真白くま
-INSERT INTO youtube_channels (vtuber_id) VALUES ('kuma');
-
---- 輝夜月
-INSERT INTO youtube_channels (vtuber_id) VALUES ('luna');
-
---- 宝鐘マリン
-INSERT INTO youtube_channels (vtuber_id) VALUES ('marine');
-
---- 夏色まつり
-INSERT INTO youtube_channels (vtuber_id) VALUES ('matsuri');
-
---- 夜空メル
-INSERT INTO youtube_channels (vtuber_id) VALUES ('mel');
-
---- さくらみこ
-INSERT INTO youtube_channels (vtuber_id) VALUES ('miko');
-
---- 大神ミオ
-INSERT INTO youtube_channels (vtuber_id) VALUES ('mio');
-
---- カグラナナ
-INSERT INTO youtube_channels (vtuber_id) VALUES ('nana');
-
---- 猫宮ひなた
-INSERT INTO youtube_channels (vtuber_id) VALUES ('nekomiya');
-
---- 白銀ノエル
-INSERT INTO youtube_channels (vtuber_id) VALUES ('noel');
-
---- 猫又おかゆ
-INSERT INTO youtube_channels (vtuber_id) VALUES ('okayu');
-
---- 兎田ぺこら
-INSERT INTO youtube_channels (vtuber_id) VALUES ('pekora');
-
---- Pinky Pop Hepburn
-INSERT INTO youtube_channels (vtuber_id) VALUES ('pph');
-
---- ロボ子さん
-INSERT INTO youtube_channels (vtuber_id) VALUES ('roboco');
-
---- 潤羽るしあ
-INSERT INTO youtube_channels (vtuber_id) VALUES ('rushia');
-
---- 紫咲シオン
-INSERT INTO youtube_channels (vtuber_id) VALUES ('shion');
-
---- ときのそら
-INSERT INTO youtube_channels (vtuber_id) VALUES ('sora');
-
---- 大空スバル
-INSERT INTO youtube_channels (vtuber_id) VALUES ('subaru');
-
---- 星街すいせい
-INSERT INTO youtube_channels (vtuber_id) VALUES ('suisei');
-
---- 犬山たまき
-INSERT INTO youtube_channels (vtuber_id) VALUES ('tamaki');
-
---- 常闇トワ
-INSERT INTO youtube_channels (vtuber_id) VALUES ('towa');
-
---- しぐれうい
-INSERT INTO youtube_channels (vtuber_id) VALUES ('ui');
-
---- 角巻わため
-INSERT INTO youtube_channels (vtuber_id) VALUES ('watame');
-
---- アキ・ローゼンタール
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('aki');
-
---- 湊あくあ
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('aqua');
-
---- 百鬼あやめ
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('ayame');
-
---- AZKi
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('azki');
-
---- 癒月ちょこ
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('choco');
-
---- 希薇娅Civia
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('civia');
-
---- 黑桃影
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('echo');
-
---- 不知火フレア
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('flare');
-
---- 白上フブキ
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('fubuki');
-
---- 赤井はあと
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('haato');
-
---- hololive
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('hololive');
-
---- 戌神ころね
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('korone');
-
---- 真白くま
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('kuma');
-
---- 輝夜月
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('luna');
-
---- 宝鐘マリン
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('marine');
-
---- 夏色まつり
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('matsuri');
-
---- 夜空メル
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('mel');
-
---- さくらみこ
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('miko');
-
---- 大神ミオ
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('mio');
-
---- カグラナナ
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('nana');
-
---- 猫宮ひなた
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('nekomiya');
-
---- 白銀ノエル
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('noel');
-
---- 猫又おかゆ
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('okayu');
-
---- 兎田ぺこら
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('pekora');
-
---- Pinky Pop Hepburn
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('pph');
-
---- ロボ子さん
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('roboco');
-
---- 潤羽るしあ
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('rushia');
-
---- 紫咲シオン
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('shion');
-
---- ときのそら
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('sora');
-
---- 大空スバル
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('subaru');
-
---- 星街すいせい
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('suisei');
-
---- 犬山たまき
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('tamaki');
-
---- しぐれうい
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('ui');
-
---- 夜霧Yogiri
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('yogiri');
-
---- 天音かなた
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('kanata');
-
---- 桐生ココ
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('coco');
-
---- 角巻わため
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('watame');
-
---- 常闇トワ
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('towa');
-
---- 姫森ルーナ
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('himemoriluna');
-
---- 朵莉丝Doris
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('doris');
-
---- 阿媂娅Artia
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('artia');
-
---- 罗莎琳Rosalyn
-INSERT INTO bilibili_channels (vtuber_id) VALUES ('rosalyn');
-
-
-INSERT INTO youtube_channels (vtuber_id) VALUES ('risu');
-INSERT INTO youtube_channels (vtuber_id) VALUES ('moona');
-INSERT INTO youtube_channels (vtuber_id) VALUES ('iofi');
-INSERT INTO youtube_channels (vtuber_id) VALUES ('rurudo');
+insert into youtube_channels (vtuber_id)
+     values ('hololive'),        -- ホロライブ公式
+            ('sora'),            -- ときのそら
+            ('roboco'),          -- ロボ子さん
+            ('miko'),            -- さくらみこ
+            ('suisei'),          -- 星街すいせい
+            ('fubuki'),          -- 白上フブキ
+            ('matsuri'),         -- 夏色まつり
+            ('haato'),           -- 赤井はあと
+            ('aki'),             -- アキロゼ
+            ('mel'),             -- 夜空メル
+            ('choco'),           -- 癒月ちょこ
+            ('choco_alt'),       -- 癒月ちょこ Sub
+            ('shion'),           -- 紫咲シオン
+            ('aqua'),            -- 湊あくあ
+            ('subaru'),          -- 大空スバル
+            ('ayame'),           -- 百鬼あやめ
+            ('pekora'),          -- 兎田ぺこら
+            ('rushia'),          -- 潤羽るしあ
+            ('flare'),           -- 不知火フレア
+            ('marine'),          -- 宝鐘マリン
+            ('noel'),            -- 白銀ノエル
+            ('kanata'),          -- 天音かなた
+            ('coco'),            -- 桐生ココ
+            ('watame'),          -- 角巻わため
+            ('towa'),            -- 常闇トワ
+            ('himemoriluna'),    -- 姫森ルーナ
+            ('lamy'),            -- 雪花ラミィ
+            ('nene'),            -- 桃鈴ねね
+            ('botan'),           -- 獅白ぼたん
+            ('aloe'),            -- 魔乃アロエ
+            ('polka'),           -- 尾丸ポルカ
+            ('mio'),             -- 大神ミオ
+            ('okayu'),           -- 猫又おかゆ
+            ('korone'),          -- 戌神ころね
+            ('azki'),            -- AZKi
+            ('risu'),            -- Ayunda Risu
+            ('moona'),           -- Moona Hoshinova
+            ('iofi'),            -- Airani Iofifteen
+            ('luna'),            -- 輝夜月
+            ('nekomiya'),        -- 猫宮ひなた
+            ('tamaki'),          -- 犬山たまき
+            ('pph'),             -- Pinky Pop Hepburn
+            ('nana'),            -- カグラナナ
+            ('ui'),              -- しぐれうい
+            ('rurudo');          -- rurudo
+
+insert into bilibili_channels (vtuber_id)
+     values ('hololive'),        -- ホロライブ公式
+            ('sora'),            -- ときのそら
+            ('roboco'),          -- ロボ子さん
+            ('miko'),            -- さくらみこ
+            ('suisei'),          -- 星街すいせい
+            ('fubuki'),          -- 白上フブキ
+            ('matsuri'),         -- 夏色まつり
+            ('haato'),           -- 赤井はあと
+            ('aki'),             -- アキロゼ
+            ('mel'),             -- 夜空メル
+            ('choco'),           -- 癒月ちょこ
+            ('shion'),           -- 紫咲シオン
+            ('aqua'),            -- 湊あくあ
+            ('subaru'),          -- 大空スバル
+            ('ayame'),           -- 百鬼あやめ
+            ('pekora'),          -- 兎田ぺこら
+            ('rushia'),          -- 潤羽るしあ
+            ('flare'),           -- 不知火フレア
+            ('marine'),          -- 宝鐘マリン
+            ('noel'),            -- 白銀ノエル
+            ('kanata'),          -- 天音かなた
+            ('coco'),            -- 桐生ココ
+            ('watame'),          -- 角巻わため
+            ('towa'),            -- 常闇トワ
+            ('himemoriluna'),    -- 姫森ルーナ
+            ('lamy'),            -- 雪花ラミィ
+            ('nene'),            -- 桃鈴ねね
+            ('botan'),           -- 獅白ぼたん
+            ('aloe'),            -- 魔乃アロエ
+            ('polka'),           -- 尾丸ポルカ
+            ('mio'),             -- 大神ミオ
+            ('okayu'),           -- 猫又おかゆ
+            ('korone'),          -- 戌神ころね
+            ('azki'),            -- AZKi
+            ('yogiri'),          -- 夜霧 Yogiri
+            ('civia'),           -- 希薇娅 Civia
+            ('echo'),            -- 黑桃影 Echo
+            ('doris'),           -- 朵莉丝 Doris
+            ('artia'),           -- 阿媂娅 Artia
+            ('rosalyn'),         -- 罗莎琳 Rosalyn
+            ('luna'),            -- 輝夜月
+            ('nekomiya'),        -- 猫宮ひなた
+            ('tamaki'),          -- 犬山たまき
+            ('pph'),             -- Pinky Pop Hepburn
+            ('nana'),            -- カグラナナ
+            ('ui');              -- しぐれうい
