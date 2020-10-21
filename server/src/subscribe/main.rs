925189989ec1511c88bfcf3a3d3208b1af5de2bc
PoiScript <poiscript@gmail.com>
2020-10-21 16:15:42 +0800

    fix(server): update pubsub callback url

--- server/src/subscribe/main.rs
+++ server/src/subscribe/main.rs
@@ -1,17 +1,15 @@
 #[path = "../error.rs"]
 mod error;
-#[path = "../requests/mod.rs"]
-mod requests;
 #[path = "../vtubers.rs"]
 mod vtubers;
 
 use futures::{stream, FutureExt, StreamExt};
-use reqwest::Client;
+use reqwest::{Client, header::CONTENT_TYPE};
 
 use crate::error::Result;
 use crate::vtubers::VTUBERS;
 
-const CALLBACK_URL: &str = concat!("https://holo.poi.cat/api/v3/", env!("PUBSUBHUBBUB_URL"));
+const CALLBACK_URL: &str = concat!("https://holo.poi.cat/api/pubsub/", env!("PUBSUBHUBBUB_URL"));
 
 const TOPIC_BASE_URL: &str = "https://www.youtube.com/xml/feeds/videos.xml?channel_id=";
 
@@ -22,7 +20,7 @@ async fn main() -> Result<()> {
     stream::iter(VTUBERS.iter().filter_map(|v| v.youtube).map(|channel_id| {
         client
             .post("https://pubsubhubbub.appspot.com/subscribe")
-            .header("Content-Type", "application/x-www-form-urlencoded")
+            .header(CONTENT_TYPE, "application/x-www-form-urlencoded")
             .body(format!(
                 "hub.callback={}&hub.topic={}{}&hub.mode=subscribe",
                 CALLBACK_URL, TOPIC_BASE_URL, channel_id
