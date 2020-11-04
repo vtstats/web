8a2624b9080628a9cd70c1321edb1b8fe3053c1c
PoiScript <poiscript@gmail.com>
2020-11-04 16:06:44 +0800

    feat(web): update api shema and components structure

--- web/src/app/shared/services/api.service.ts
+++ web/src/app/shared/services/api.service.ts
@@ -3,44 +3,49 @@ import { HttpClient, HttpParams } from "@angular/common/http";
 import { Observable } from "rxjs";
 
 import {
+  ChannelListOption,
   ChannelListResponse,
+  ChannelReportOption,
   ChannelReportResponse,
-  StreamReportResponse,
+  StreamListOption,
   StreamListResponse,
+  StreamReportOption,
+  StreamReportResponse,
 } from "src/app/models";
 
-import { ConfigService } from "./config.service";
-
-const BASE_URL = "https://holo.poi.cat/api/v3";
+const BASE_URL = "https://holo.poi.cat/api/v4";
 
 @Injectable({ providedIn: "root" })
 export class ApiService {
-  constructor(private http: HttpClient, private config: ConfigService) {}
+  constructor(private http: HttpClient) {}
 
-  getYouTubeChannels(): Observable<ChannelListResponse> {
+  youtubeChannels(opts: ChannelListOption): Observable<ChannelListResponse> {
     return this.http.get<ChannelListResponse>(`${BASE_URL}/youtube_channels`, {
-      params: new HttpParams().set("ids", [...this.config.vtuber].join(",")),
+      params: new HttpParams().set("ids", opts.ids.join(",")),
     });
   }
 
-  getBilibiliChannels(): Observable<ChannelListResponse> {
+  bilibiliChannels(opts: ChannelListOption): Observable<ChannelListResponse> {
     return this.http.get<ChannelListResponse>(`${BASE_URL}/bilibili_channels`, {
-      params: new HttpParams().set("ids", [...this.config.vtuber].join(",")),
+      params: new HttpParams().set("ids", opts.ids.join(",")),
     });
   }
 
-  getYouTubeStreams(
-    ids: string[],
-    options: { startAt?: Date; endAt?: Date } = {}
-  ): Observable<StreamListResponse> {
-    let params = new HttpParams().set("ids", ids.join(","));
+  youtubeStreams(opts: StreamListOption): Observable<StreamListResponse> {
+    let params = new HttpParams()
+      .set("ids", opts.ids.join(","))
+      .set("status", opts.status.join(","));
 
-    if (options.startAt) {
-      params = params.set("startAt", options.startAt.toISOString());
+    if (opts.orderBy) {
+      params = params.set("orderBy", opts.orderBy);
     }
 
-    if (options.endAt) {
-      params = params.set("endAt", options.endAt.toISOString());
+    if (opts.startAt) {
+      params = params.set("startAt", Number(opts.startAt).toString());
+    }
+
+    if (opts.endAt) {
+      params = params.set("endAt", Number(opts.endAt).toString());
     }
 
     return this.http.get<StreamListResponse>(`${BASE_URL}/youtube_streams`, {
@@ -48,49 +53,35 @@ export class ApiService {
     });
   }
 
-  getYouTubeScheduleStream(): Observable<StreamListResponse> {
-    return this.http.get<StreamListResponse>(
-      `${BASE_URL}/youtube_schedule_streams`,
-      {
-        params: new HttpParams().set("ids", [...this.config.vtuber].join(",")),
-      }
-    );
-  }
+  channelReports(opts: ChannelReportOption): Observable<ChannelReportResponse> {
+    let params = new HttpParams()
+      .set("ids", opts.ids.join(","))
+      .set("metrics", opts.metrics.join(","));
 
-  getChannelReport(
-    ids: string,
-    metrics: string,
-    startAt: Date,
-    endAt: Date
-  ): Observable<ChannelReportResponse> {
-    const params = new HttpParams({
-      fromObject: {
-        ids,
-        metrics,
-        startAt: startAt.toISOString(),
-        endAt: endAt.toISOString(),
-      },
-    });
+    if (opts.startAt) {
+      params = params.set("startAt", Number(opts.startAt).toString());
+    }
+
+    if (opts.endAt) {
+      params = params.set("endAt", Number(opts.endAt).toString());
+    }
 
     return this.http.get<ChannelReportResponse>(`${BASE_URL}/channels_report`, {
       params,
     });
   }
 
-  getStreamReport(
-    ids: string,
-    metrics: string = "youtube_stream_viewer",
-    startAt?: Date,
-    endAt?: Date
-  ): Observable<StreamReportResponse> {
-    let params = new HttpParams().set("ids", ids).set("metrics", metrics);
+  streamReports(opts: StreamReportOption): Observable<StreamReportResponse> {
+    let params = new HttpParams()
+      .set("ids", opts.ids.join(","))
+      .set("metrics", opts.metrics.join(","));
 
-    if (startAt) {
-      params = params.set("startAt", startAt.toISOString());
+    if (opts.startAt) {
+      params = params.set("startAt", Number(opts.startAt).toString());
     }
 
-    if (endAt) {
-      params = params.set("endAt", endAt.toISOString());
+    if (opts.endAt) {
+      params = params.set("endAt", Number(opts.endAt).toString());
     }
 
     return this.http.get<StreamReportResponse>(`${BASE_URL}/streams_report`, {
--- web/src/app/shared/shared.module.ts
+++ web/src/app/shared/shared.module.ts
@@ -2,37 +2,30 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { MatTableModule } from "@angular/material/table";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { MatIconModule } from "@angular/material/icon";
 import { NgxChartsModule } from "@swimlane/ngx-charts";
 import { EllipsisModule } from "ngx-ellipsis";
-
-import { AreaChartComponent } from "./components/area-chart.component";
-import { ShimmerStreamListComponent } from "./components/shimmer.stream-list.component";
-import { ShimmerSubmenuComponent } from "./components/shimmer-submenu.component";
-import { ShimmerTableComponent } from "./components/shimmer-table.component";
-import { StreamItemComponent } from "./components/stream-item.component";
 
 import { ColoredNumberDirective } from "./directives/colored-number.directive";
 
 import { DistancePipe } from "./pipes/distance.pipe";
 import { DurationPipe } from "./pipes/duration.pipe";
+import { FormatISOPipe } from "./pipes/format-iso.pipe";
+import { GroupByPipe } from "./pipes/group-by.pipe";
 import { NamePipe } from "./pipes/name.pipe";
 
 import { ApiService } from "./services/api.service";
-import { ConfigService } from "./services/config.service";
 import { TickService } from "./services/tick.service";
 
 @NgModule({
   declarations: [
     DistancePipe,
     DurationPipe,
+    FormatISOPipe,
+    GroupByPipe,
     NamePipe,
-    AreaChartComponent,
-    ShimmerTableComponent,
-    ShimmerStreamListComponent,
-    ShimmerSubmenuComponent,
     ColoredNumberDirective,
-    StreamItemComponent,
   ],
   imports: [
     CommonModule,
@@ -40,19 +33,17 @@ import { TickService } from "./services/tick.service";
     EllipsisModule,
     MatTableModule,
     MatIconModule,
+    MatProgressSpinnerModule,
     NgxChartsModule,
   ],
   exports: [
     DistancePipe,
     DurationPipe,
+    FormatISOPipe,
+    GroupByPipe,
     NamePipe,
-    AreaChartComponent,
-    ShimmerTableComponent,
-    ShimmerStreamListComponent,
-    ShimmerSubmenuComponent,
     ColoredNumberDirective,
-    StreamItemComponent,
   ],
-  providers: [ApiService, ConfigService, TickService],
+  providers: [ApiService, TickService],
 })
 export class SharedModule {}
