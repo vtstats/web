ed1ada35a90955e7fc4c42c429ed776a9779401b
PoiScript <poiscript@gmail.com>
2020-11-16 15:14:17 +0800

    feat(server): tls feature

--- server/src/api/main.rs
+++ server/src/api/main.rs
@@ -63,7 +63,18 @@ async fn main() -> Result<()> {
         .and_then(|addr| addr.parse().ok())
         .unwrap_or_else(|| ([127, 0, 0, 1], 4200).into());
 
-    warp::serve(routes).run(addr).await;
+    cfg_if::cfg_if! {
+        if #[cfg(feature = "tls")] {
+            warp::serve(routes)
+                .tls()
+                .cert_path(env::var("SSL_CERT_PATH").unwrap())
+                .key_path(env::var("SSL_KEY_PATH").unwrap())
+                .run(addr)
+                .await;
+        } else {
+            warp::serve(routes).run(addr).await;
+        }
+    }
 
     Ok(())
 }
