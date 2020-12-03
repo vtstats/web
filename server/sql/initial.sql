4d87ecfd5ff45c3c5cc69f72de8a3fc6b755b847
Awoocado <carlos.bullon99@gmail.com>
2020-12-03 10:54:11 -0500

    adding semicolons because i'm blind

--- server/sql/initial.sql
+++ server/sql/initial.sql
@@ -37,8 +37,8 @@ insert into youtube_channels (vtuber_id)
             ('moona'),           -- Moona Hoshinova
             ('iofi'),            -- Airani Iofifteen
             ('ollie'),           -- Kureiji Ollie
-            ('melfissa')         -- Anya Melfissa
-            ('reine')            -- Pavolia Reine
+            ('melfissa'),        -- Anya Melfissa
+            ('reine'),           -- Pavolia Reine
             ('amelia'),          -- Watson Amelia
             ('calliope'),        -- Mori Calliope
             ('gura'),            -- Gawr Gura
