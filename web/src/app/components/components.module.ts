9eb8eef7c06e23ef39fc18f8978320ba803012a7
PoiScript <poiscript@gmail.com>
2021-02-07 01:27:37 +0800

    feat(web): switch to apexcharts

--- web/src/app/components/components.module.ts
+++ web/src/app/components/components.module.ts
@@ -6,10 +6,13 @@ import { MatTableModule } from "@angular/material/table";
 import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
 import { MatSortModule } from "@angular/material/sort";
 import { MatIconModule } from "@angular/material/icon";
+import { MatButtonModule } from "@angular/material/button";
 import { NgxChartsModule } from "@swimlane/ngx-charts";
 import { EllipsisModule } from "ngx-ellipsis";
 
-import { AreaChart } from "./area-chart/area-chart";
+import { ApxChart } from "./apx-chart/apx-chart";
+import { ChannelStats } from "./channel-stats/channel-stats";
+import { ChannelStatsChart } from "./channel-stats-chart/channel-stats-chart";
 import { BigNumber } from "./big-number/big-number";
 import { ChannelTable } from "./channel-table/channel-table";
 import { LoadingSpinner } from "./loading-spinner/loading-spinner";
@@ -19,6 +22,7 @@ import { ShimmerStreamItem } from "./shimmer-stream-item/shimmer-stream-item";
 import { StreamItem } from "./stream-item/stream-item";
 import { StreamGroup } from "./stream-group/stream-group";
 import { StreamsList } from "./stream-list/stream-list";
+import { StreamStatsChart } from "./stream-stats-chart/stream-stats-chart";
 import { StreamsSummary } from "./stream-summary/stream-summary";
 import { SubMenu, SubMenuTitle, SubMenuExtra } from "./sub-menu/sub-menu";
 import { VTubeSummary } from "./vtuber-summary/vtuber-summary";
@@ -27,7 +31,9 @@ import { SharedModule } from "src/app/shared";
 
 @NgModule({
   declarations: [
-    AreaChart,
+    ApxChart,
+    ChannelStats,
+    ChannelStatsChart,
     BigNumber,
     ChannelTable,
     LoadingSpinner,
@@ -37,6 +43,7 @@ import { SharedModule } from "src/app/shared";
     ShimmerStreamItem,
     StreamItem,
     StreamsList,
+    StreamStatsChart,
     StreamsSummary,
     SubMenu,
     SubMenuTitle,
@@ -49,6 +56,7 @@ import { SharedModule } from "src/app/shared";
     PortalModule,
     NgxChartsModule,
     EllipsisModule,
+    MatButtonModule,
     MatProgressSpinnerModule,
     MatTableModule,
     MatIconModule,
@@ -56,8 +64,8 @@ import { SharedModule } from "src/app/shared";
     SharedModule,
   ],
   exports: [
-    AreaChart,
     BigNumber,
+    ChannelStats,
     ChannelTable,
     LoadingSpinner,
     NumberRow,
@@ -66,6 +74,7 @@ import { SharedModule } from "src/app/shared";
     ShimmerStreamItem,
     StreamItem,
     StreamsList,
+    StreamStatsChart,
     StreamsSummary,
     SubMenu,
     SubMenuTitle,
