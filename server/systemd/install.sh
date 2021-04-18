166dbb8ec238d7cec2cd79bcd2b256fa66470887
linnil1 <linnil1.886@gmail.com>
2021-04-19 01:48:00 +0800

    Update RSS script

--- server/systemd/install.sh
+++ server/systemd/install.sh
@@ -9,10 +9,13 @@ systemctl enable holostats_stream_stat
 systemctl enable holostats_stream_stat.timer
 systemctl enable holostats_channel_stat
 systemctl enable holostats_channel_stat.timer
+systemctl enable holostats_rss
+systemctl enable holostats_rss.timer
 
 systemctl start holostats_server
 systemctl start holostats_stream_stat.timer
 systemctl start holostats_channel_stat.timer
+systemctl start holostats_rss.timer
 
 systemctl status holostats_*
 systemctl list-timers
