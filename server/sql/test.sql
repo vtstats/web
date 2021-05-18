dad7b337b4ea5f7041b57e5c8f6b7f4ddf56d4ae
linnil1 <linnil1.886@gmail.com>
2021-05-19 00:40:28 +0800

    [TaiwanV] Exclude the test in modification

--- server/sql/test.sql
+++ server/sql/test.sql
@@ -12,20 +12,20 @@ $$ language plpgsql;
 update youtube_channels
    set (updated_at, view_count,daily_view_count, weekly_view_count, monthly_view_count)
      = (:today - interval '30 day', 1000, 0, 0, 0)
- where vtuber_id = 'miru';
+ where vtuber_id = 'ayame';
 
 insert into youtube_channel_view_statistic (vtuber_id, time, value)
-     values ('miru', :today - interval '32 day', 100),
-            ('miru', :today - interval '8 day', 400),
-            ('miru', :today - interval '2 day', 800);
+     values ('ayame', :today - interval '32 day', 100),
+            ('ayame', :today - interval '8 day', 400),
+            ('ayame', :today - interval '2 day', 800);
 
 do $$
 declare
     youtube_channel youtube_channels%rowtype;
 begin
-    select * from youtube_channels into youtube_channel where vtuber_id = 'miru';
+    select * from youtube_channels into youtube_channel where vtuber_id = 'ayame';
 
-    assert(youtube_channel.vtuber_id = 'miru');
+    assert(youtube_channel.vtuber_id = 'ayame');
     assert(youtube_channel.view_count = 1000);
     assert(youtube_channel.daily_view_count = 1000 - 800);
     assert(youtube_channel.weekly_view_count = 1000 - 400);
@@ -34,7 +34,7 @@ end;
 $$;
 
 insert into youtube_streams (stream_id, title, vtuber_id, updated_at, status)
-     values ('poi', 'Stream Title', 'miru', :today, 'ended');
+     values ('poi', 'Stream Title', 'ayame', :today, 'ended');
 
 insert into youtube_stream_viewer_statistic (stream_id, value, time)
      values ('poi', 100, :today),
