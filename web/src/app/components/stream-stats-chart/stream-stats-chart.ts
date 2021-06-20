1b8b82f834e56e56d1271fe15e13d39b5cc76ec5
PoiScript <poiscript@gmail.com>
2021-06-20 16:30:54 +0800

    feat(web): stream detail styling

--- web/src/app/components/stream-stats-chart/stream-stats-chart.ts
+++ web/src/app/components/stream-stats-chart/stream-stats-chart.ts
@@ -11,7 +11,7 @@ import { format } from "date-fns";
 
 import { ChannelReportKind, Report } from "src/app/models";
 import { ApxChart } from "src/app/components/apx-chart/apx-chart";
-import { translate, getLocaleId } from "src/i18n";
+import { getLocaleId } from "src/i18n";
 
 const custom = ({ series, seriesIndex, dataPointIndex, w }) => {
   return (
@@ -24,7 +24,17 @@ const custom = ({ series, seriesIndex, dataPointIndex, w }) => {
 @Component({
   selector: "hs-stream-stats-chart",
   template: `
-    <span>{{ title }}</span>
+    <hs-sub-menu>
+      <hs-sub-menu-title>
+        <span i18n="@@streamViewers">Recent Streams</span>
+      </hs-sub-menu-title>
+
+      <hs-sub-menu-extra>
+        <time i18n="@@updatedAt" [attr.datetime]="updatedAt | formatISO">
+          Updated at {{ updatedAt | date: "MM/dd HH:mm" }}
+        </time>
+      </hs-sub-menu-extra>
+    </hs-sub-menu>
     <apx-chart #chart [height]="350"></apx-chart>
   `,
   styleUrls: ["stream-stats-chart.scss"],
@@ -35,12 +45,8 @@ const custom = ({ series, seriesIndex, dataPointIndex, w }) => {
 export class StreamStatsChart implements OnChanges {
   @Input() report: Report<ChannelReportKind>;
 
-  get title(): string {
-    return translate("streamViewers");
-  }
-
-  get colors(): string[] {
-    return ["#e00404"];
+  get updatedAt(): number {
+    return this.report.rows[this.report.rows.length - 1][0];
   }
 
   @ViewChild("chart", { static: true })
@@ -87,13 +93,13 @@ export class StreamStatsChart implements OnChanges {
         enabled: false,
       },
       chart: {
-        id: this.title,
+        id: "streamViewer",
         type: "area",
         height: 350,
         toolbar: { show: false },
         zoom: { enabled: false },
       },
-      colors: this.colors,
+      colors: ["#e00404"],
     });
   }
 }
@@ -101,7 +107,15 @@ export class StreamStatsChart implements OnChanges {
 @Component({
   selector: "hs-stream-stats-chart-shimmer",
   template: `
-    <span class="text shimmer" [style.width.px]="90"></span>
+    <hs-sub-menu>
+      <hs-sub-menu-title>
+        <span class="text shimmer" [style.width.px]="90"></span>
+      </hs-sub-menu-title>
+
+      <hs-sub-menu-extra>
+        <span class="shimmer text" [style.width.px]="120"></span>
+      </hs-sub-menu-extra>
+    </hs-sub-menu>
     <div class="shimmer" [style.height.px]="350"></div>
   `,
   encapsulation: ViewEncapsulation.None,
