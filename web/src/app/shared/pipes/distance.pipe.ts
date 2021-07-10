8ab5e4d2934b337292ca6ca26d76bdcc60692c3d
PoiScript <poiscript@gmail.com>
2021-07-10 23:16:47 +0800

    fix(web): catch falsy value in DistancePipe

--- web/src/app/shared/pipes/distance.pipe.ts
+++ web/src/app/shared/pipes/distance.pipe.ts
@@ -6,6 +6,8 @@ import { getDateFnsLocale } from "src/i18n";
 @Pipe({ name: "distance" })
 export class DistancePipe implements PipeTransform {
   transform(start: number | Date, end: number | Date): string {
+    if (!start || !end) return null;
+
     return formatDistanceStrict(start, end, {
       locale: getDateFnsLocale(),
       addSuffix: true,
