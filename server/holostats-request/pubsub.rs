0c956eb6c07596fc933d22fc196db07b7d2710f5
PoiScript <poiscript@gmail.com>
2021-08-22 16:16:16 +0800

    feat(server): include hub.secret when subscribing

--- server/holostats-request/pubsub.rs
+++ server/holostats-request/pubsub.rs
@@ -14,13 +14,19 @@ impl RequestHub {
     pub async fn subscribe_youtube_pubsub(&self, channel_id: &str) -> Result<()> {
         const TOPIC_BASE_URL: &str = "https://www.youtube.com/xml/feeds/videos.xml?channel_id=";
 
+        let body = format!(
+            "hub.callback=https://{}/api/pubsub/{}&hub.topic={}{}&hub.mode=subscribe&hub.secret={}",
+            CONFIG.server.hostname,
+            CONFIG.youtube.pubsub_path,
+            TOPIC_BASE_URL,
+            channel_id,
+            CONFIG.youtube.pubsub_secret
+        );
+
         self.client
             .post("https://pubsubhubbub.appspot.com/subscribe")
             .header(CONTENT_TYPE, "application/x-www-form-urlencoded")
-            .body(format!(
-                "hub.callback=https://{}/api/pubsub/{}&hub.topic={}{}&hub.mode=subscribe",
-                CONFIG.server.hostname, CONFIG.youtube.pubsub_path, TOPIC_BASE_URL, channel_id
-            ))
+            .body(body)
             .send()
             .await
             .and_then(Response::error_for_status)
