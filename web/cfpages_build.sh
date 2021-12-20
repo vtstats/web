cbb0dbffc239c8ec761ef5752a23e2212afb60a3
PoiScript <poiscript@gmail.com>
2021-12-20 14:33:20 +0800

    build(web): add build.sh

---
+++ web/cfpages_build.sh
@@ -0,0 +1,15 @@
+export PATH="$PATH:$(yarn global bin)"
+
+echo "export const environment = {
+  production: true,
+  commit_sha: '$CF_PAGES_COMMIT_SHA',
+  branch: '$CF_PAGES_BRANCH',
+};" > src/environments/environment.prod.ts
+
+yarn build --configuration production
+
+yarn global add @sentry/cli
+sentry-cli releases new $CF_PAGES_COMMIT_SHA
+sentry-cli releases set-commits $CF_PAGES_COMMIT_SHA --commit "PoiScript/HoloStats@$CF_PAGES_COMMIT_SHA"
+sentry-cli releases deploys $CF_PAGES_COMMIT_SHA new -e $CF_PAGES_BRANCH
+sentry-cli releases finalize $CF_PAGES_COMMIT_SHA
