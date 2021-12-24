0cf03110d472ae7cb1139622f56668f8cefdd918
PoiScript <poiscript@gmail.com>
2021-12-24 13:16:10 +0800

    feat(web): update stream-stats-chart

--- web/src/app/components/stream-stats-chart/stream-stats-chart.ts
+++ web/src/app/components/stream-stats-chart/stream-stats-chart.ts
@@ -3,14 +3,11 @@ import {
   Component,
   ElementRef,
   Input,
-  NgZone,
   OnDestroy,
   OnInit,
   ViewChild,
   ViewEncapsulation,
 } from "@angular/core";
-import { lightFormat } from "date-fns";
-import SVG from "svg.js";
 import { fromEvent, Subscription } from "rxjs";
 import {
   debounceTime,
@@ -18,10 +15,13 @@ import {
   map,
   distinctUntilChanged,
 } from "rxjs/operators";
-import { ScaleLinear, scaleLinear, scaleBand, ScaleBand } from "d3-scale";
+import { ScaleLinear, scaleLinear } from "d3-scale";
+import { bisectCenter, extent, max, range } from "d3-array";
+import { area, curveLinear, line } from "d3-shape";
 
 import { PopperComponent } from "../popper/popper";
 import { Stream } from "src/app/models";
+import { truncateTo15Seconds, within } from "src/utils";
 
 @Component({
   selector: "hs-stream-stats-chart",
@@ -30,7 +30,7 @@ import { Stream } from "src/app/models";
   encapsulation: ViewEncapsulation.None,
 })
 export class StreamStatsChart implements OnInit, OnDestroy {
-  constructor(private ngZone: NgZone, private host: ElementRef<HTMLElement>) {}
+  constructor(private host: ElementRef<HTMLElement>) {}
 
   @Input() streamId: string;
 
@@ -39,18 +39,8 @@ export class StreamStatsChart implements OnInit, OnDestroy {
   @ViewChild("popperComp")
   popperComp: PopperComponent;
 
-  popper = {
-    idx: 0,
-    date: 0,
-    value: 0,
-  };
-
-  @ViewChild("bars", { static: true })
-  private lineEl: ElementRef;
-  private lineSvg: SVG.Doc;
-
-  private guideLine: SVG.Line;
-  private dataPoint: SVG.Circle;
+  @ViewChild("svg", { static: true })
+  svg: ElementRef<HTMLElement>;
 
   readonly dataPointSize: number = 5;
   readonly height: number = 300;
@@ -59,14 +49,20 @@ export class StreamStatsChart implements OnInit, OnDestroy {
   readonly topMargin: number = 10;
   readonly bottomMargin: number = 30;
 
-  @Input() rows: [number, number][];
   @Input() stream: Stream;
+  @Input("rows") _raw: [number, number][] = [];
 
+  rows: [number, number][] = this._raw;
+  xScale: ScaleLinear<number, number> = scaleLinear();
   yScale: ScaleLinear<number, number> = scaleLinear();
-  max = 300;
-  step = 2;
-  timeUnit = 15_000;
-  fitContent = true;
+  ticks: number[] = [];
+  areaPath: string;
+  linePath: string;
+  width: number = 0;
+  dataPointIdx: number = -1;
+
+  step = 3;
+  unit: number | "fit" = "fit";
 
   sub: Subscription;
 
@@ -76,286 +72,137 @@ export class StreamStatsChart implements OnInit, OnDestroy {
     // TODO: switch to resize observer
     this.sub = fromEvent(window, "resize")
       .pipe(
-        filter(() => this.fitContent),
+        filter(() => this.unit == "fit"),
         map(() => this.host.nativeElement.getBoundingClientRect().width),
         distinctUntilChanged(),
         debounceTime(500)
       )
-      .subscribe((w) => this._render(w));
+      .subscribe((w) => {
+        this._render();
+      });
   }
 
   ngOnDestroy() {
-    if (this.lineSvg) {
-      this.lineSvg.off("mousemove");
-      this.lineSvg.off("mouseleave");
-      this.lineSvg.clear();
-    }
     if (this.sub) {
       this.sub.unsubscribe();
     }
   }
 
-  formatTimeUnit(unit: number): string {
+  formatTimeUnit(unit: number | "fit"): string {
+    if (unit === "fit") return "Fit";
     return unit >= 60000 ? `${unit / 60000}m` : `${unit / 1000}s`;
   }
 
-  _render(w?: number) {
-    if (!w) {
-      w = this.host.nativeElement.getBoundingClientRect().width;
-    }
+  _render() {
+    const hostWidth = this.host.nativeElement.getBoundingClientRect().width;
 
-    const len = this.rows.length - 1;
+    if (typeof this.unit === "number") {
+      this.rows = this._raw.filter(
+        ([time]) => truncateTo15Seconds(time) % (this.unit as number) == 0
+      );
+      this.width = Math.max(this.rows.length * this.step, hostWidth);
 
-    const width = w - (this.leftMargin + this.rightMargin);
-
-    if (this.fitContent) {
-      const unit = Math.ceil((len * 2) / width);
-
-      this.timeUnit = unit * 15000;
-      this.step = +((width * unit) / len).toFixed(2);
-    }
-
-    this.step = +((width * this.timeUnit) / 15000 / len).toFixed(2);
-
-    this.step = Math.max(2, this.step);
-
-    const rows = this._agg();
-    this.max = Math.max(...rows.map((r) => r[1]));
-    this.yScale.domain([0, this.max]).range([0, this.height]);
-
-    this.ngZone.runOutsideAngular(() => this._drawSvg(rows));
-  }
-
-  private _agg(): [number, number][] {
-    const ret = [];
-    const sorted = this.rows.sort((a, b) => a[0] - b[0]);
-    const step = this.timeUnit / 15_000;
-
-    for (let index = 0; index < sorted.length; index += step) {
-      const element = sorted[index];
-      ret.push(element);
-    }
-
-    return ret;
-  }
-
-  private _drawSvg(rows: [number, number][]) {
-    if (this.lineSvg) {
-      this.lineSvg.off("mousemove");
-      this.lineSvg.off("mouseleave");
-      this.lineSvg.off("touchmove");
-      this.lineSvg.off("dblclick");
-      this.lineSvg.clear();
+      console.log(
+        `[stream-chat] unit: ${this.unit} raw: ${this._raw.length} rows: ${this.rows.length}`
+      );
     } else {
-      this.lineSvg = SVG(this.lineEl.nativeElement);
-    }
+      const len = this._raw.length;
 
-    this.lineSvg.size(
-      this.leftMargin + (rows.length - 1) * this.step + this.rightMargin,
-      this.topMargin + this.height + this.bottomMargin
-    );
+      const unit = Math.ceil((len * 2) / hostWidth);
 
-    // line and gradient
-    {
-      const points = [];
+      this.rows = this._raw.filter(
+        ([time]) => truncateTo15Seconds(time) % unit == 0
+      );
+      this.width = hostWidth;
 
-      for (const [idx, [at, v]] of rows.entries()) {
-        const h = this.yScale(v);
-        points.push([
-          this.leftMargin + idx * this.step,
-          this.topMargin + this.height - h,
-        ]);
-      }
-
-      const line = this.lineSvg
-        .polyline(points.map((p) => [p[0], this.topMargin + this.height]))
-        .fill("none")
-        .stroke({
-          color: "rgb(234, 67, 53)",
-          width: 2,
-          dasharray: "none",
-        });
-
-      (line as any).animate(300).plot(points);
-
-      const linear = this.lineSvg
-        .gradient("linear", function (stop) {
-          stop.at({ offset: 0, color: "#EA4335", opacity: 0.38 });
-          stop.at({ offset: 0.9, color: "#FCDEE5", opacity: 0 });
-        })
-        .from(0, 0)
-        .to(0, 1);
-
-      const path = this.lineSvg
-        .path([
-          ["M", this.leftMargin, this.topMargin + this.height],
-          ...points.map((p) => ["L", p[0], this.topMargin + this.height]),
-          [
-            "L",
-            this.leftMargin + this.step * (rows.length - 1),
-            this.topMargin + this.height,
-          ],
-        ])
-        .fill(linear);
-
-      (path as any)
-        .animate(300)
-        .plot([
-          ["M", this.leftMargin, this.topMargin + this.height],
-          ...points.map((p) => ["L", p[0], p[1]]),
-          [
-            "L",
-            this.leftMargin + this.step * (rows.length - 1),
-            this.topMargin + this.height,
-          ],
-        ]);
-    }
-
-    // labels
-    {
-      const inc = Math.ceil(60 / this.step);
-
-      for (let index = 1; index < rows.length; index += inc) {
-        this.lineSvg
-          .line([
-            this.leftMargin + index * this.step,
-            this.topMargin + this.height,
-            this.leftMargin + index * this.step,
-            this.topMargin + this.height + 6,
-          ])
-          .addClass("line")
-          .stroke({ width: 1, color: "#E6E6E6" });
-
-        this.lineSvg
-          .text(lightFormat(rows[index][0], "HH:mm"))
-          .move(
-            this.leftMargin + index * this.step,
-            this.topMargin + this.height + 8
-          )
-          .addClass("x label")
-          .font({ anchor: "middle", family: "Fira Code" });
-      }
-    }
-
-    // guide line and data point
-    {
-      this.guideLine = this.lineSvg
-        .line([
-          [0, 0],
-          [0, this.topMargin + this.height],
-        ])
-        .addClass("guideline")
-        .stroke({
-          width: 2,
-          linecap: "round",
-          dasharray: "1.5, 6",
-        })
-        .fill("noone")
-        .move(-2434, 0);
-
-      this.dataPoint = this.lineSvg
-        .circle(this.dataPointSize * 2)
-        .fill("rgb(234, 67, 53)")
-        .move(-2434, 0);
-    }
-
-    this.lineSvg.on("mousemove", (e: MouseEvent) =>
-      this._handleMousemove(e, rows)
-    );
-
-    this.lineSvg.on("mouseleave", this._handleMouseleave, this);
-
-    this.lineSvg.on("touchmove", (e: TouchEvent) =>
-      this._handleTouchmove(e, rows)
-    );
-
-    this.lineSvg.on("dblclick", (e) => this._handleDblClick(e, rows), this);
-  }
-
-  _handleMousemove(e: MouseEvent, rows: [number, number][]) {
-    if (
-      e.target instanceof Element &&
-      ["svg", "path"].includes(e.target.tagName.toLowerCase())
-    ) {
-      const x =
-        this.lineEl.nativeElement.scrollLeft +
-        e.clientX -
-        this.lineEl.nativeElement.getBoundingClientRect().x -
-        this.leftMargin;
-
-      this._updateGuideline(x, rows);
-    }
-  }
-
-  _handleTouchmove(e: TouchEvent, rows: [number, number][]) {
-    const x =
-      this.lineEl.nativeElement.scrollLeft +
-      e.touches[0].clientX -
-      this.lineEl.nativeElement.getBoundingClientRect().x;
-
-    const currentX = this.guideLine.attr("x1");
-
-    if (Math.abs(x - currentX) > 30) return;
-
-    if (e.cancelable) {
-      e.preventDefault();
-      this._updateGuideline(x - this.leftMargin, rows);
-    }
-  }
-
-  _handleDblClick(e: MouseEvent, rows: [number, number][]) {
-    if (this.stream.status !== "ended") return;
-
-    const x =
-      this.lineEl.nativeElement.scrollLeft +
-      e.clientX -
-      this.lineEl.nativeElement.getBoundingClientRect().x -
-      this.leftMargin;
-
-    const idx = Math.round(x / this.step);
-
-    if (0 <= idx && idx <= rows.length - 1) {
-      window.open(
-        `https://youtu.be/${this.stream.streamId}?t=${Math.max(
-          0,
-          Math.round((rows[idx][0] - this.stream.startTime) / 1000)
-        )}s`,
-        "_blank"
+      console.log(
+        `[stream-chat] unit: ${unit}(fit) raw: ${this._raw.length} rows: ${this.rows.length}`
       );
     }
+
+    this.xScale
+      .domain(extent(this.rows, (row) => row[0]))
+      .range([this.leftMargin, this.width - this.rightMargin]);
+
+    this.yScale
+      .domain([0, max(this.rows, (row) => row[1])])
+      .range([this.height + this.topMargin, this.topMargin]);
+
+    this.areaPath = area()
+      .curve(curveLinear)
+      .x((d) => this.xScale(d[0]))
+      .y0(this.yScale(0))
+      .y1((d) => this.yScale(d[1]))(this.rows);
+
+    this.linePath = line()
+      .curve(curveLinear)
+      .x((d) => this.xScale(d[0]))
+      .y((d) => this.yScale(d[1]))(this.rows);
+
+    this.ticks = range(1, this.rows.length - 1, 32);
   }
 
-  _updateGuideline(x: number, rows: [number, number][]) {
-    const idx = Math.min(
-      Math.max(0, Math.round(x / this.step)),
-      rows.length - 1
+  _handleMousemove(e: MouseEvent) {
+    const { x, y, top, left } = this.svg.nativeElement.getBoundingClientRect();
+
+    const offsetX = e.clientX - x;
+    const offsetY = e.clientY - y;
+
+    const idx = bisectCenter(
+      this.rows.map((x) => x[0]),
+      this.xScale.invert(offsetX)
     );
 
-    if (this.popper.idx === idx) return;
-
-    const [d, v] = rows[idx];
-
-    const h = this.yScale(v);
-
-    this.guideLine.move(this.leftMargin + idx * this.step, 0);
-    this.dataPoint.move(
-      this.leftMargin + idx * this.step - this.dataPointSize,
-      this.topMargin + this.height - h - this.dataPointSize
-    );
-
-    this.ngZone.run(() => {
-      this.popper.idx = idx;
-      this.popper.date = d;
-      this.popper.value = v;
-    });
-
-    // popper reposition
-    this.popperComp.update(this.dataPoint.node);
+    if (
+      within(idx, 0, this.rows.length - 1) &&
+      within(offsetY, this.topMargin, this.topMargin + this.height)
+    ) {
+      this.dataPointIdx = idx;
+      const x = left + this.xScale(this.rows[idx][0]);
+      const y = top + this.yScale(this.rows[idx][1]);
+      this.popperComp.update({
+        getBoundingClientRect: () => ({
+          width: 0,
+          height: 0,
+          right: x,
+          left: x,
+          top: y,
+          bottom: y,
+        }),
+      } as Element);
+    } else {
+      this.hidePopper();
+    }
   }
 
-  _handleMouseleave() {
-    this.guideLine.move(-2434, 0);
-    this.dataPoint.move(-2434, 0);
+  _handleTouchmove(e: TouchEvent) {}
+
+  _handleDblClick(e: MouseEvent) {
+    if (this.stream.status !== "ended") return;
+
+    const { x, y } = this.svg.nativeElement.getBoundingClientRect();
+
+    const offsetX = e.clientX - x;
+    const offsetY = e.clientY - y;
+
+    const idx = bisectCenter(
+      this.rows.map((x) => x[0]),
+      this.xScale.invert(offsetX)
+    );
+
+    if (
+      within(idx, 0, this.rows.length - 1) &&
+      within(offsetY, this.topMargin, this.topMargin + this.height) &&
+      this.rows[idx][0] > this.stream.startTime
+    ) {
+      const t = Math.round((this.rows[idx][0] - this.stream.startTime) / 1000);
+
+      window.open(`https://youtu.be/${this.stream.streamId}?t=${t}s`, "_blank");
+    }
+  }
+
+  hidePopper() {
+    this.dataPointIdx = -1;
     this.popperComp.hide();
   }
 }
