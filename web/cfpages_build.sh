c513f252d4546166b654d667eecddc870115258a
PoiScript <poiscript@gmail.com>
2022-04-13 12:51:20 +0800

    chore(web): update cfpages_build.sh

--- web/cfpages_build.sh
+++ web/cfpages_build.sh
@@ -13,8 +13,11 @@ yarn build --configuration production
 
 mv dist/safety-worker.js dist/ngsw-worker.js
 
-yarn global add @sentry/cli
-sentry-cli releases new $CF_PAGES_COMMIT_SHA
-sentry-cli releases set-commits $CF_PAGES_COMMIT_SHA --commit "PoiScript/HoloStats@$CF_PAGES_COMMIT_SHA"
-sentry-cli releases deploys $CF_PAGES_COMMIT_SHA new -e $CF_PAGES_BRANCH
-sentry-cli releases finalize $CF_PAGES_COMMIT_SHA
+# create a deploy in sentry
+if [ "$CF_PAGES_BRANCH" = "master" ] || [ "$CF_PAGES_BRANCH" = "dev" ]; then
+  yarn global add @sentry/cli
+  sentry-cli releases new $CF_PAGES_COMMIT_SHA
+  sentry-cli releases set-commits $CF_PAGES_COMMIT_SHA --commit "PoiScript/HoloStats@$CF_PAGES_COMMIT_SHA"
+  sentry-cli releases deploys $CF_PAGES_COMMIT_SHA new -e $CF_PAGES_BRANCH
+  sentry-cli releases finalize $CF_PAGES_COMMIT_SHA
+fi
