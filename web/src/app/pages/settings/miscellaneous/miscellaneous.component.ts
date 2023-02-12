e6c9d905c06ec55b7ca57bd2f9635d78538601d0
PoiScript <poiscript@gmail.com>
2023-02-12 17:43:53 +0800

    feat(web): toggle react query devtools

--- web/src/app/pages/settings/miscellaneous/miscellaneous.component.ts
+++ web/src/app/pages/settings/miscellaneous/miscellaneous.component.ts
@@ -1,6 +1,8 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { MatListModule } from "@angular/material/list";
 import { RouterModule } from "@angular/router";
+
+import { QryService } from "src/app/shared/qry";
 
 @Component({
   standalone: true,
@@ -8,4 +10,6 @@ import { RouterModule } from "@angular/router";
   selector: "hls-miscellaneous",
   templateUrl: "./miscellaneous.component.html",
 })
-export class MiscellaneousComponent {}
+export class MiscellaneousComponent {
+  qry = inject(QryService);
+}
