6aa7dab1529bb37a7ea875dcd1acf43f6f941f7f
PoiScript <poiscript@gmail.com>
2023-07-26 23:05:42 +0800

    feat: add hololive english advent

--- server/sql/initial.sql
+++ server/sql/initial.sql
@@ -1,131 +1,261 @@
-insert into youtube_channels (vtuber_id)
-     values ('hololive'),        -- ホロライブ公式
-            ('hololive_en'),     -- Hololive English
-            ('hololive_id'),     -- Hololive Indonesia
-            ('sora'),            -- ときのそら
-            ('roboco'),          -- ロボ子さん
-            ('miko'),            -- さくらみこ
-            ('suisei'),          -- 星街すいせい
-            ('fubuki'),          -- 白上フブキ
-            ('matsuri'),         -- 夏色まつり
-            ('haato'),           -- 赤井はあと
-            ('aki'),             -- アキロゼ
-            ('mel'),             -- 夜空メル
-            ('choco'),           -- 癒月ちょこ
-            ('choco_alt'),       -- 癒月ちょこ Sub
-            ('shion'),           -- 紫咲シオン
-            ('aqua'),            -- 湊あくあ
-            ('subaru'),          -- 大空スバル
-            ('ayame'),           -- 百鬼あやめ
-            ('pekora'),          -- 兎田ぺこら
-            ('rushia'),          -- 潤羽るしあ
-            ('flare'),           -- 不知火フレア
-            ('marine'),          -- 宝鐘マリン
-            ('noel'),            -- 白銀ノエル
-            ('kanata'),          -- 天音かなた
-            ('coco'),            -- 桐生ココ
-            ('watame'),          -- 角巻わため
-            ('towa'),            -- 常闇トワ
-            ('himemoriluna'),    -- 姫森ルーナ
-            ('lamy'),            -- 雪花ラミィ
-            ('nene'),            -- 桃鈴ねね
-            ('botan'),           -- 獅白ぼたん
-            ('chloe'),           -- 沙花叉クロヱ
-            ('iroha'),           -- 風真いろは
-            ('koyori'),          -- 博衣こより
-            ('laplus'),          -- ラプラス・ダークネス
-            ('lui'),             -- 鷹嶺ルイ
-            ('polka'),           -- 尾丸ポルカ
-            ('mio'),             -- 大神ミオ
-            ('okayu'),           -- 猫又おかゆ
-            ('korone'),          -- 戌神ころね
-            ('azki'),            -- AZKi
-            ('risu'),            -- Ayunda Risu
-            ('moona'),           -- Moona Hoshinova
-            ('iofi'),            -- Airani Iofifteen
-            ('ollie'),           -- Kureiji Ollie
-            ('melfissa'),        -- Anya Melfissa
-            ('reine'),           -- Pavolia Reine
-            ('vestia'),          -- Vestia Zeta
-            ('kaela'),           -- Kaela Kovalskia
-            ('kobo'),            -- Kobo Kanaeru
-            ('amelia'),          -- Watson Amelia
-            ('calliope'),        -- Mori Calliope
-            ('gura'),            -- Gawr Gura
-            ('inanis'),          -- Ninomae Ina'nis
-            ('kiara'),           -- Takanashi Kiara
-            ('irys'),            -- IRyS
-            ('luna'),            -- 輝夜月
-            ('nekomiya'),        -- 猫宮ひなた
-            ('tamaki'),          -- 犬山たまき
-            ('pph'),             -- Pinky Pop Hepburn
-            ('nana'),            -- カグラナナ
-            ('ui'),              -- しぐれうい
-            ('rurudo'),          -- rurudo
-            ('miyabi'),          -- 花咲みやび
-            ('izuru'),           -- 奏手イヅル
-            ('aruran'),          -- アルランディス
-            ('rikka'),           -- 律可
-            ('astel'),           -- アステル・レダ
-            ('temma'),           -- 岸堂天真
-            ('roberu'),          -- 夕刻ロベル
-            ('shien'),           -- 影山シエン
-            ('oga'),             -- 荒咬オウガ
-            ('fuma'),            -- 夜十神封魔
-            ('uyu'),             -- 羽継烏有
-            ('gamma'),           -- 緋崎ガンマ
-            ('rio'),             -- 水無世燐央
-            ('axel_syrios'),
-            ('magni_dezmond'),
-            ('noir_vesper'),
-            ('regis_altare'),
-            ('gavis_bettel'),
-            ('machina_x_flayon'),
-            ('banzoin_hakka'),
-            ('josuiji_shinri');
+insert into
+     youtube_channels (vtuber_id)
+values
+     ('hololive'),
+     -- ホロライブ公式
+     ('hololive_en'),
+     -- Hololive English
+     ('hololive_id'),
+     -- Hololive Indonesia
+     ('sora'),
+     -- ときのそら
+     ('roboco'),
+     -- ロボ子さん
+     ('miko'),
+     -- さくらみこ
+     ('suisei'),
+     -- 星街すいせい
+     ('fubuki'),
+     -- 白上フブキ
+     ('matsuri'),
+     -- 夏色まつり
+     ('haato'),
+     -- 赤井はあと
+     ('aki'),
+     -- アキロゼ
+     ('mel'),
+     -- 夜空メル
+     ('choco'),
+     -- 癒月ちょこ
+     ('choco_alt'),
+     -- 癒月ちょこ Sub
+     ('shion'),
+     -- 紫咲シオン
+     ('aqua'),
+     -- 湊あくあ
+     ('subaru'),
+     -- 大空スバル
+     ('ayame'),
+     -- 百鬼あやめ
+     ('pekora'),
+     -- 兎田ぺこら
+     ('rushia'),
+     -- 潤羽るしあ
+     ('flare'),
+     -- 不知火フレア
+     ('marine'),
+     -- 宝鐘マリン
+     ('noel'),
+     -- 白銀ノエル
+     ('kanata'),
+     -- 天音かなた
+     ('coco'),
+     -- 桐生ココ
+     ('watame'),
+     -- 角巻わため
+     ('towa'),
+     -- 常闇トワ
+     ('himemoriluna'),
+     -- 姫森ルーナ
+     ('lamy'),
+     -- 雪花ラミィ
+     ('nene'),
+     -- 桃鈴ねね
+     ('botan'),
+     -- 獅白ぼたん
+     ('chloe'),
+     -- 沙花叉クロヱ
+     ('iroha'),
+     -- 風真いろは
+     ('koyori'),
+     -- 博衣こより
+     ('laplus'),
+     -- ラプラス・ダークネス
+     ('lui'),
+     -- 鷹嶺ルイ
+     ('polka'),
+     -- 尾丸ポルカ
+     ('mio'),
+     -- 大神ミオ
+     ('okayu'),
+     -- 猫又おかゆ
+     ('korone'),
+     -- 戌神ころね
+     ('azki'),
+     -- AZKi
+     ('risu'),
+     -- Ayunda Risu
+     ('moona'),
+     -- Moona Hoshinova
+     ('iofi'),
+     -- Airani Iofifteen
+     ('ollie'),
+     -- Kureiji Ollie
+     ('melfissa'),
+     -- Anya Melfissa
+     ('reine'),
+     -- Pavolia Reine
+     ('vestia'),
+     -- Vestia Zeta
+     ('kaela'),
+     -- Kaela Kovalskia
+     ('kobo'),
+     -- Kobo Kanaeru
+     ('amelia'),
+     -- Watson Amelia
+     ('calliope'),
+     -- Mori Calliope
+     ('gura'),
+     -- Gawr Gura
+     ('inanis'),
+     -- Ninomae Ina'nis
+     ('kiara'),
+     -- Takanashi Kiara
+     ('irys'),
+     -- IRyS
+     ('luna'),
+     -- 輝夜月
+     ('nekomiya'),
+     -- 猫宮ひなた
+     ('tamaki'),
+     -- 犬山たまき
+     ('pph'),
+     -- Pinky Pop Hepburn
+     ('nana'),
+     -- カグラナナ
+     ('ui'),
+     -- しぐれうい
+     ('rurudo'),
+     -- rurudo
+     ('miyabi'),
+     -- 花咲みやび
+     ('izuru'),
+     -- 奏手イヅル
+     ('aruran'),
+     -- アルランディス
+     ('rikka'),
+     -- 律可
+     ('astel'),
+     -- アステル・レダ
+     ('temma'),
+     -- 岸堂天真
+     ('roberu'),
+     -- 夕刻ロベル
+     ('shien'),
+     -- 影山シエン
+     ('oga'),
+     -- 荒咬オウガ
+     ('fuma'),
+     -- 夜十神封魔
+     ('uyu'),
+     -- 羽継烏有
+     ('gamma'),
+     -- 緋崎ガンマ
+     ('rio'),
+     -- 水無世燐央
+     ('axel_syrios'),
+     ('magni_dezmond'),
+     ('noir_vesper'),
+     ('regis_altare'),
+     ('gavis_bettel'),
+     ('machina_x_flayon'),
+     ('banzoin_hakka'),
+     -- Hololive English -Advent-
+     ('josuiji_shinri'),
+     ('koseki_bijou'),
+     ('shiori_novella'),
+     ('fuwamoco'),
+     ('nerissa_ravencroft');
 
-insert into bilibili_channels (vtuber_id)
-     values ('hololive'),        -- ホロライブ公式
-            ('sora'),            -- ときのそら
-            ('roboco'),          -- ロボ子さん
-            ('miko'),            -- さくらみこ
-            ('suisei'),          -- 星街すいせい
-            ('fubuki'),          -- 白上フブキ
-            ('matsuri'),         -- 夏色まつり
-            ('haato'),           -- 赤井はあと
-            ('aki'),             -- アキロゼ
-            ('mel'),             -- 夜空メル
-            ('choco'),           -- 癒月ちょこ
-            ('shion'),           -- 紫咲シオン
-            ('aqua'),            -- 湊あくあ
-            ('subaru'),          -- 大空スバル
-            ('ayame'),           -- 百鬼あやめ
-            ('pekora'),          -- 兎田ぺこら
-            ('rushia'),          -- 潤羽るしあ
-            ('flare'),           -- 不知火フレア
-            ('marine'),          -- 宝鐘マリン
-            ('noel'),            -- 白銀ノエル
-            ('kanata'),          -- 天音かなた
-            ('coco'),            -- 桐生ココ
-            ('watame'),          -- 角巻わため
-            ('towa'),            -- 常闇トワ
-            ('himemoriluna'),    -- 姫森ルーナ
-            ('lamy'),            -- 雪花ラミィ
-            ('nene'),            -- 桃鈴ねね
-            ('botan'),           -- 獅白ぼたん
-            ('polka'),           -- 尾丸ポルカ
-            ('mio'),             -- 大神ミオ
-            ('okayu'),           -- 猫又おかゆ
-            ('korone'),          -- 戌神ころね
-            ('azki'),            -- AZKi
-            ('amelia'),          -- Watson Amelia
-            ('calliope'),        -- Mori Calliope
-            ('gura'),            -- Gawr Gura
-            ('inanis'),          -- Ninomae Ina'nis
-            ('kiara'),           -- Takanashi Kiara
-            ('luna'),            -- 輝夜月
-            ('nekomiya'),        -- 猫宮ひなた
-            ('tamaki'),          -- 犬山たまき
-            ('pph'),             -- Pinky Pop Hepburn
-            ('nana'),            -- カグラナナ
-            ('ui');              -- しぐれうい
+insert into
+     bilibili_channels (vtuber_id)
+values
+     ('hololive'),
+     -- ホロライブ公式
+     ('sora'),
+     -- ときのそら
+     ('roboco'),
+     -- ロボ子さん
+     ('miko'),
+     -- さくらみこ
+     ('suisei'),
+     -- 星街すいせい
+     ('fubuki'),
+     -- 白上フブキ
+     ('matsuri'),
+     -- 夏色まつり
+     ('haato'),
+     -- 赤井はあと
+     ('aki'),
+     -- アキロゼ
+     ('mel'),
+     -- 夜空メル
+     ('choco'),
+     -- 癒月ちょこ
+     ('shion'),
+     -- 紫咲シオン
+     ('aqua'),
+     -- 湊あくあ
+     ('subaru'),
+     -- 大空スバル
+     ('ayame'),
+     -- 百鬼あやめ
+     ('pekora'),
+     -- 兎田ぺこら
+     ('rushia'),
+     -- 潤羽るしあ
+     ('flare'),
+     -- 不知火フレア
+     ('marine'),
+     -- 宝鐘マリン
+     ('noel'),
+     -- 白銀ノエル
+     ('kanata'),
+     -- 天音かなた
+     ('coco'),
+     -- 桐生ココ
+     ('watame'),
+     -- 角巻わため
+     ('towa'),
+     -- 常闇トワ
+     ('himemoriluna'),
+     -- 姫森ルーナ
+     ('lamy'),
+     -- 雪花ラミィ
+     ('nene'),
+     -- 桃鈴ねね
+     ('botan'),
+     -- 獅白ぼたん
+     ('polka'),
+     -- 尾丸ポルカ
+     ('mio'),
+     -- 大神ミオ
+     ('okayu'),
+     -- 猫又おかゆ
+     ('korone'),
+     -- 戌神ころね
+     ('azki'),
+     -- AZKi
+     ('amelia'),
+     -- Watson Amelia
+     ('calliope'),
+     -- Mori Calliope
+     ('gura'),
+     -- Gawr Gura
+     ('inanis'),
+     -- Ninomae Ina'nis
+     ('kiara'),
+     -- Takanashi Kiara
+     ('luna'),
+     -- 輝夜月
+     ('nekomiya'),
+     -- 猫宮ひなた
+     ('tamaki'),
+     -- 犬山たまき
+     ('pph'),
+     -- Pinky Pop Hepburn
+     ('nana'),
+     -- カグラナナ
+     ('ui');
+
+-- しぐれうい
